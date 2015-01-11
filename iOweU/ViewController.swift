0f1109cfe021204c3b6618a1d47427849b3dea44
Phoenix7351 <Phoenix@Justins-Mac-mini.local>
2015-01-11 11:12:16 -0600

    Creating fresh project

---
+++ iOweU/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOweU
+//
+//  Created by Justin Tiffner on 1/11/15.
+//  Copyright (c) 2015 Justin Tiffner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
